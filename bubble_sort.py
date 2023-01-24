eb00bf4f045119471417663c8c5c3353f024bb54
abby-moana <abbymoana1@gmail.com>
2023-01-24 20:43:02 +0800

    temp variable and nums of j variable

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,6 +2,8 @@ def sort(nums):
     for i in range(len(nums) -1, 0, -1):
         for j in range(i):
             if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
 
 
 

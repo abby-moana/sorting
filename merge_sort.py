e90fcfb41af78a4438c5528e2c92ebae7ec2fc4d
abby-moana <abbymoana1@gmail.com>
2023-01-25 00:29:24 +0800

    merge left nums and merge right nums

--- merge_sort.py
+++ merge_sort.py
@@ -3,6 +3,12 @@ def merge(nums):
         left_nums = nums[:len(nums)//2]
         right_nums = nums[len(nums)//2:]
 
+        merge(left_nums)
+        merge(right_nums)
+
+
+
+
 nums = [3, 7, 20, 47, 77, 82, 18, 72, 54, 96]
 merge(nums)
 

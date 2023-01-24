8b8a3ef6c68608636bdfcf2c3907e7501b5ab6ca
abby-moana <abbymoana1@gmail.com>
2023-01-24 20:20:12 +0800

    if statement

--- selection_sort.py
+++ selection_sort.py
@@ -2,7 +2,8 @@ def sort(nums):
     for i in range(9):
         minpos = i
         for j in range(i, 10):
-
+            if nums[j] < nums[minpos]:
+                minpos
 
 nums = [3, 7, 20, 47, 77, 82, 18, 72, 54, 96]
 sort(nums)

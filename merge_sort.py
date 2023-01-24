75cac1acdc52f81331d26b55d6fcf82d96e9b533
abby-moana <abbymoana1@gmail.com>
2023-01-25 00:28:23 +0800

    left nums and right nums variable

--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,7 @@
 def merge(nums):
     if len(nums) > 1:
-        l
-
+        left_nums = nums[:len(nums)//2]
+        right_nums = nums[len(nums)//2:]
 
 nums = [3, 7, 20, 47, 77, 82, 18, 72, 54, 96]
 merge(nums)
